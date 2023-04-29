4e32cd9b1af2568cc4ca12f1a38d2cbe82d164c3
majiang <majiang@users.noreply.github.com>
2023-04-29 18:45:16 +0900

    separate capital and small letters

--- src/woff2svgs.py
+++ src/woff2svgs.py
@@ -15,10 +15,16 @@ def woff_to_svg(woff_file_path, output_directory):
     
     # グリフのデータをSVGとして出力する
     for (glyph_name, glyph_data) in zip(font.getGlyphOrder(), svg_table.docList):
+        if len(glyph_name) == 1:
+            if 'a' <= glyph_name <= 'z':
+                glyph_name += "-small"
+            if 'A' <= glyph_name <= 'Z':
+                glyph_name += "-capital"
         file_name = f"{glyph_name}.svg"
         output_file_path = os.path.join(output_directory, file_name)
         with open(output_file_path, "w") as svg_file:
             svg_file.write(glyph_data.data)
+        logger.info(glyph_name)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
