70161bd7508a634b2d5c6e0cb7f296af78b65fda
majiang <majiang@users.noreply.github.com>
2023-05-02 23:24:55 +0900

    SVGs with thinner frame

--- src/manipulate.py
+++ src/manipulate.py
@@ -1,3 +1,4 @@
+import dataclasses
 import logging
 import os
 import re
@@ -21,20 +22,40 @@ def calculate_bounding_box(path):
             max_y = max(max_y, segment.start.imag, segment.end.imag)
     return min_x, min_y, max_x - min_x, max_y - min_y
 
-def calculate_view_box(path):
+def calculate_view_box(path, shrink):
     min_x, min_y, width, height = calculate_bounding_box(path)
     assert min_x == int(min_x)
     assert min_y == int(min_y)
     assert width == int(width)
     assert height == int(height)
-    return f'{int(min_x)} {int(min_y)} {int(width)} {int(height)}'
+    return f'{int(min_x)+shrink} {int(min_y)+shrink} {int(width)-shrink*2} {int(height)-shrink*2}'
 
-def main(input_folder, output_folder):
+@dataclasses.dataclass
+class PathModification:
+    from_path_d_path: str
+    to_path_d_path: str
+    shrink: int
+
+def main(
+        input_folder: str,
+        output_folder: str,
+        path_modification: PathModification|None=None
+        ):
+    shrink = 0
+    if path_modification:
+        from_path_d = open(path_modification.from_path_d_path).read().strip()
+        to_path_d = ' '.join(line.strip() for line in open(path_modification.to_path_d_path))
+        shrink = path_modification.shrink
+
     ret = {}
     os.makedirs(output_folder, exist_ok=True)
 
     for filename in os.listdir(input_folder):
         if not filename.endswith('.svg'):
+            continue
+        if filename.endswith('.vert.svg'):
+            continue
+        if not filename.startswith('u1F'):
             continue
         input_path = os.path.join(input_folder, filename)
         output_path = os.path.join(output_folder, filename)
@@ -42,7 +63,7 @@ def main(input_folder, output_folder):
         logger.info(filename)
         if 'space' in filename or 'uni3000' in filename:
             continue
-        view_box = calculate_view_box(input_path)
+        view_box = calculate_view_box(input_path, shrink)
         logger.info(view_box)
 
         if view_box not in ret:
@@ -55,7 +76,10 @@ def main(input_folder, output_folder):
         if 'viewBox' in svg_content:
             raise ValueError(f'viewBox already exists: {filename}')
 
-        svg_content = svg_content.replace('<svg', f'<svg viewBox="{view_box}"').replace("><", ">\n<")
+        svg_content = svg_content.replace('<svg', f'<svg viewBox="{view_box}"')
+        svg_content = svg_content.replace("><", ">\n<")
+        if path_modification:
+            svg_content = svg_content.replace(from_path_d, to_path_d)
 
         with open(output_path, 'w') as file:
             file.write(svg_content)
@@ -66,4 +90,13 @@ def main(input_folder, output_folder):
     ))
 
 logging.basicConfig(level=logging.INFO, format="%(lineno)d\t%(message)s")
-main("original/woff-svg", "svg/from-woff")
+for shrink in (10, 20):
+    main(
+        "original/woff-svg",
+        f"svg/from-woff/frame-{30-shrink}",
+        PathModification(
+        from_path_d_path="value/frame.original-30.path-d",
+        to_path_d_path=f"value/frame.{30-shrink}.path-d",
+        shrink=shrink))
+
+main("original/woff-svg", "svg/from-woff/original")
